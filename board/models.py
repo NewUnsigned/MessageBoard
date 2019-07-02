0b8b7043a64d3c58c0db7901d1d0d54e2af25d69
bimuyu <bimuyu@helijia.com>
2019-07-02 14:29:17 +0800

    add Message

---
+++ board/models.py
@@ -0,0 +1,24 @@
+'''
+  File: models.py
+  Project: board
+  
+  Created Date: 2019-07-02 10:22:25
+  Author: zhaopeng
+  -----
+  Copyright (c) 2019 zhaopeng. All rights reserved.
+  -----
+  HISTORY:
+  Date      	By		Comments
+  ----------	---		---------------------------------------------------------
+'''
+
+from datetime import datetime
+from board import db
+from flask_sqlalchemy import SQLAlchemy
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(200))
+    name = db.Column(db.String(20))
+    timestamp = db.Column(db.DateTime, defaut=datetime.now, index=True)
+    
